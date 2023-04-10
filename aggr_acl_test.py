73dc85010fd43cb4207c0e305012346446af4537
batman <pgrisales@unal.edu.co>
2023-04-09 23:10:01 -0500

    aggr_acl_test solicitante done

--- aggr_acl_test.py
+++ aggr_acl_test.py
@@ -1,1 +1,41 @@
-# usar click_solicitud de aggr_acl_lib
+import time
+from lib.aggr_acl_lib import click_solicitud, write_acl
+from login_test import login_test as login
+from getpass import getpass
+import lib.shared_lib as shared
+from edit_sol_test import edit_sol_test
+
+def aggr_acl(driver, keyword, acl):
+  shared.select_role(driver, 'Solicitante')
+
+  UAC = 0
+  passed = 0
+
+  try:
+    shared.select_module(driver, 'Mis solicitudes')
+    time.sleep(5)
+    
+    shared.search(driver, 'Solicitudes', keyword)
+    time.sleep(5)
+
+    click_solicitud(driver, 0)
+    time.sleep(5)
+
+    write_acl(driver, acl)
+    time.sleep(5)
+
+    print(f'aggr_acl_test COMPLETED: {passed}/{UAC} UAC PASSED')
+  except Exception as e:
+    traceback.print_exc()
+    print(f'aggr_acl_test FAILED: {passed}/{UAC} UAC PASSED')
+
+if __name__ == '__main__':
+  driver = shared.init_driver()
+  u = 'pgrisales'
+  p = 'Tengoalza99'
+  #login(driver, input('Username: '), getpass('Password: '))
+  login(driver, u, p)
+  aggr_acl(driver, keyword='1512', acl='prueba 1234')
+
+
+
--- lib/aggr_acl_lib.py
+++ lib/aggr_acl_lib.py
@@ -1,11 +1,26 @@
 from selenium.webdriver.common.by import By
 
 def click_solicitud(driver, idx):
-    table = driver.find_element(By.XPATH, f"//div[contains(text(), 'Mis Solicitudes')]/following-sibling::div//table")
-    tbody = table.find_element(By.TAG_NAME, 'tbody')
-    rows = tbody.find_elements(By.TAG_NAME, 'tr')
-    if len(rows) > 0:
-        row = rows[idx]
-        row.click()
-    else:
-        raise Exception('No records found')
+  table = driver.find_element(By.XPATH, f"//div[contains(text(), 'Mis Solicitudes')]/following-sibling::div//table")
+  tbody = table.find_element(By.TAG_NAME, 'tbody')
+  rows = tbody.find_elements(By.TAG_NAME, 'tr')
+  if len(rows) > 0:
+    row = rows[idx]
+    row.click()
+  else:
+    raise Exception('No records found')
+
+def write_acl(driver, acl):
+  # Input field xpath
+  input_path= '/html/body/div/div/div/div[1]/div[3]/div/div[1]/div[2]/div/form/div[2]/div/div[7]/div/div/div/div[1]/div[1]/textarea'
+  input_field = driver.find_element(By.XPATH, input_path)
+
+  # Type text into the input field
+  input_field.send_keys(acl)
+
+  # Input field xpath
+  b_path = '/html/body/div/div/div/div[1]/div[3]/div/div[1]/div[2]/div/form/div[3]/button[2]'
+  b = driver.find_element(By.XPATH, b_path)
+  # Save acl
+  b.click()
+
