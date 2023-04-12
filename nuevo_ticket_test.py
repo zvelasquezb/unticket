f4aa72b3579204fa6284025446cdfcf11f7109f2
zamir <37085970+zvelasquezb@users.noreply.github.com>
2023-04-12 01:19:24 -0500

    1st

--- nuevo_ticket_test.py
+++ nuevo_ticket_test.py
@@ -1,1 +1,47 @@
-# similar a todos los aggr test : se necesita función para validar URL de pagos, para eso hay que cambiar de tab
+import time
+import lib.aggr_acl_lib as aggr_acl
+from login_test import login_test as login
+from getpass import getpass
+import lib.shared_lib as shared
+import utils.filters as filters
+import traceback
+
+# Test de agregar aclaracion - solicitante
+def aggr_acl_test(driver, id, respuesta):
+
+  UAC = 1
+  passed = 0
+
+  try:
+    # Add clarification
+    shared.select_module(driver, 'Mis solicitudes')
+    time.sleep(5)
+    
+    shared.search(driver, 'Mis Solicitudes', id)
+    time.sleep(5)
+    
+    aggr_acl.click_solicitud(driver, 0)
+    time.sleep(5)
+
+    shared.enter_textarea_value(driver, 'Respuesta', respuesta)
+    shared.click_button(driver, 'Guardar')
+    time.sleep(5)
+  
+    # Check if request's state has changed 
+    shared.search(driver, 'Mis Solicitudes', id)
+    result = shared.UAC_validate_saved_record(driver, 'Mis Solicitudes', [id, 'En trámite'], 0)
+    passed += shared.evaluate_UAC_result(result)
+
+    print(f'AGGR ACL: {passed}/{UAC} UAC PASSED')
+  except Exception as e:
+    traceback.print_exc()
+    print(f'AGGR ACL: {passed}/{UAC} UAC PASSED')
+
+if __name__ == '__main__':
+  driver = shared.init_driver()
+  login(driver, input('Username: '), getpass('Password: '))
+  shared.select_role(driver, 'Solicitante')
+  #aggr_acl_test(driver, id='1512', respuesta='aclaración 1512')
+  aggr_acl_test(driver, id='1508', respuesta='aclaración 1508')
+
+
